4dc4012440e25a6891dd4a8afbb39dc2385d5b87
Romuald Brillout <git@brillout.com>
2022-12-20 12:09:26 +0100

    min

--- install.sh
+++
@@ -1,9 +0,0 @@
-echo "cloning crouton clipboard!"
-git clone http://github.com/zwhitchcox/crouton-clipboard ~/.crouton-clipboard
-touch ~/.crouton-clipboard/data.txt
-echo "adding startup script to .bashrc"
-echo "(nohup node ~/.crouton-clipboard/server.js > /dev/null 2>&1 &)                                                                          " >> $HOME/.bashrc
-echo "adding copy/paste commands to vimrc"
-echo "nnoremap \"*p :r !cat $HOME/.crouton-clipboard/data.txt<CR>\nvnoremap \"*y :'<,'>w! $HOME/.crouton-clipboard/data.txt<CR>" >> ~/.vimrc
-echo "starting crouton clipboard!"
-(nohup node ~/.crouton-clipboard/server.js > /dev/null 2>&1 &)                                                                          
